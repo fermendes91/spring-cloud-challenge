6c5cb8a497b76f7b4fa394af01ded9ceca9362b7
Fernando Mendes <fernando.mendes91@outlook.com>
2022-03-11 12:06:50 -0300

    Configuração a mais de propertie sleuth

--- eats-distancia-service/src/main/java/br/com/caelum/eats/distancia/EatsDistanciaServiceApplication.java
+++ eats-distancia-service/src/main/java/br/com/caelum/eats/distancia/EatsDistanciaServiceApplication.java
@@ -2,12 +2,18 @@ package br.com.caelum.eats.distancia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.h2.H2ConsoleAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 public class EatsDistanciaServiceApplication {
 
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EatsDistanciaServiceApplication.class, args);
 	}
--- eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/EatsPagamentoServiceApplication.java
+++ eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/EatsPagamentoServiceApplication.java
@@ -3,11 +3,19 @@ package br.com.caelum.eats.pagamento;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableFeignClients
 public class EatsPagamentoServiceApplication {
 
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EatsPagamentoServiceApplication.class, args);
 	}
