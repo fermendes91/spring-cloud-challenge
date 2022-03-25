3d8269c32950ca363364f12dd1efdbf2492ecf9c
Fernando Mendes <fernando.mendes91@outlook.com>
2022-03-25 19:16:14 -0300

    Configurando novas rotas de autenticação vinculados a perfis de acesso ( desafio desejavel )

--- eats-monolito/eats-application/src/main/java/br/com/caelum/eats/ResourceServerConfigurer.java
+++ eats-monolito/eats-application/src/main/java/br/com/caelum/eats/ResourceServerConfigurer.java
@@ -12,7 +12,13 @@ public class ResourceServerConfigurer extends ResourceServerConfigurerAdapter {
 	public void configure(HttpSecurity http) throws Exception {
 				
 		http.authorizeRequests().antMatchers(HttpMethod.POST, "/parceiros/restaurantes/**").hasRole("RESTAURANTE")
-			.antMatchers(HttpMethod.PUT, "/parceiros/restaurantes/**").hasRole("RESTAURANTE");
+			.antMatchers(HttpMethod.PUT, "/parceiros/restaurantes/**").hasRole("RESTAURANTE")
+			.antMatchers("/admin/**").hasRole("ADMIN").anyRequest().authenticated()
+			.antMatchers("/pedidos/**").hasRole("ADMIN").anyRequest().authenticated()
+			.antMatchers(HttpMethod.POST, "/parceiros/restaurantes/**").hasRole("ADMIN").anyRequest().authenticated()
+			.antMatchers(HttpMethod.PUT, "/parceiros/restaurantes/**").hasRole("ADMIN").anyRequest().authenticated()
+			.antMatchers("/pedidos/**").hasRole("CLIENTE").anyRequest().authenticated();
+			
 
 	}
 
